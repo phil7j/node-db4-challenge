e85cdc509aa5fe09106bb123da1c04664ce23fe1
Philip Johnson <phil7j@gmail.com>
2019-08-15 14:31:33 -0700

    finish config, and added all tables, migration complete

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,22 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/recipes.db3',
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+    // sqlite will not enforce Foreign Keys by default
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); // turns on the FK enforcement
+      },
+    },
   },
 
   staging: {
