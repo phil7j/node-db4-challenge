c0647327fd7e07403b25f3246854365c4692afd1
Philip Johnson <phil7j@gmail.com>
2019-08-15 18:18:11 -0700

    finish all end points and data access points, stretch complete

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -10,13 +10,18 @@ module.exports = {
   };
 
 function getRecipes(){
-    db(recipes)
+    return db('recipes')
 }
 
 function getShoppingList(recipe_id){
-
+    return db('recipe_ingredients as ri')
+        .select('i.name as Ingredient', 'ri.quantity', 'r.recipe_name as Recipe')
+        .innerJoin('ingredients as i', 'ri.ingredient_id', '=', 'i.id')
+        .innerJoin('recipes as r', 'ri.recipe_id', '=', 'r.id')
+        .where({recipe_id})
 }
 
 function getInstructions(recipe_id){
-
+    return db('recipes')
+        .where({id: recipe_id})
 }
